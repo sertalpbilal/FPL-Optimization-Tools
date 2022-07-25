0c37697efc197a1778ba5090fb42adc3ef77c135
prmac <paul.mcadam@protonmail.com>
2022-07-25 21:19:19 +0100

    add command line options for required files

--- run/solve_regular.py
+++ run/solve_regular.py
@@ -3,13 +3,24 @@ import sys
 import pathlib
 import json
 import datetime
+import argparse
 
 if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--team", help = "json file from https://fantasy.premierleague.com/api/my-team/MY_TEAM_ID/")
+    parser.add_argument("-s", "--settings", help = "json settings file as described here https://github.com/prmac/FPL-Optimization-Tools/blob/main/run/regular_settings.json")
+    parser.add_argument("-p", "--projections", help = "csv file of projections downloaded from FPLReview")
+
+    args = parser.parse_args()
+    team = Path(args.team)
+    settings = Path(args.settings)
+    projections = Path(args.projections)
+    
     base_folder = pathlib.Path()
     sys.path.append(str(base_folder / "../src"))
     from multi_period_dev import connect, get_my_data, prep_data, solve_multi_period_fpl
 
-    with open('regular_settings.json') as f:
+    with open(settings)) as f:
         options = json.load(f)
 
     if options.get("cbc_path") != "":
@@ -22,7 +33,7 @@ if __name__=="__main__":
         if session is None and team_id is None:
             exit(0)
     else:
-        with open('team.json') as f:
+        with open(team) as f:
             my_data = json.load(f)
     data = prep_data(my_data, options)
 
