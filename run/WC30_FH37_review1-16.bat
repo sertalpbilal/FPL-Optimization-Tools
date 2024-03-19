5282fdacc333a15130e70d1da147e51d1ce23db2
Erik Knudsen Garcia <ek0611@gmail.com>
2024-03-19 21:10:17 +0100

    added fplreview files for 16 permutations, added bat files

---
+++ run/WC30_FH37_review1-16.bat
@@ -0,0 +1,16 @@
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review1
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review2
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review3
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review4
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review5
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review5
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review7
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review8
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review9
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review10
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review11
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review12
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review13
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review14
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review15
+python .\solve_regular.py --use_wc 30 --use_fh 37 --datasource review16
