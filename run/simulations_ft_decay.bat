6cb8f64740946ef759f591770cb896d522939f1f
Erik Knudsen Garcia <ek0611@gmail.com>
2024-04-02 23:37:39 +0200

    new review file

--- run/simulations_ft_decay.bat
+++ run/simulations_ft_decay.bat
@@ -1,31 +1,19 @@
 python .\simulations.py --datasource review1 --ft_value 1.0 --decay_base 0.8 --no 50 --parallel 10
 python .\simulations.py --datasource review1 --ft_value 1.5 --decay_base 0.8 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 1.7 --decay_base 0.8 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 1.9 --decay_base 0.8 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 2.1 --decay_base 0.8 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 2.5 --decay_base 0.8 --no 50 --parallel 10
+python .\simulations.py --datasource review1 --ft_value 2.0 --decay_base 0.8 --no 50 --parallel 10
 python .\simulations.py --datasource review1 --ft_value 3.0 --decay_base 0.8 --no 50 --parallel 10
 
 python .\simulations.py --datasource review1 --ft_value 1.0 --decay_base 0.9 --no 50 --parallel 10
 python .\simulations.py --datasource review1 --ft_value 1.5 --decay_base 0.9 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 1.7 --decay_base 0.9 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 1.9 --decay_base 0.9 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 2.1 --decay_base 0.9 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 2.5 --decay_base 0.9 --no 50 --parallel 10
+python .\simulations.py --datasource review1 --ft_value 2.0 --decay_base 0.9 --no 50 --parallel 10
 python .\simulations.py --datasource review1 --ft_value 3.0 --decay_base 0.9 --no 50 --parallel 10
 
 python .\simulations.py --datasource review1 --ft_value 1.0 --decay_base 1.0 --no 50 --parallel 10
 python .\simulations.py --datasource review1 --ft_value 1.5 --decay_base 1.0 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 1.7 --decay_base 1.0 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 1.9 --decay_base 1.0 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 2.1 --decay_base 1.0 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 2.5 --decay_base 1.0 --no 50 --parallel 10
+python .\simulations.py --datasource review1 --ft_value 2.0 --decay_base 1.0 --no 50 --parallel 10
 python .\simulations.py --datasource review1 --ft_value 3.0 --decay_base 1.0 --no 50 --parallel 10
 
 python .\simulations.py --datasource review1 --ft_value 1.0 --decay_base 1.017 --no 50 --parallel 10
 python .\simulations.py --datasource review1 --ft_value 1.5 --decay_base 1.017 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 1.7 --decay_base 1.017 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 1.9 --decay_base 1.017 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 2.1 --decay_base 1.017 --no 50 --parallel 10
-python .\simulations.py --datasource review1 --ft_value 2.5 --decay_base 1.017 --no 50 --parallel 10
+python .\simulations.py --datasource review1 --ft_value 2.0 --decay_base 1.017 --no 50 --parallel 10
 python .\simulations.py --datasource review1 --ft_value 3.0 --decay_base 1.017 --no 50 --parallel 10
