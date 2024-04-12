c16d48d7529a4b73b50f7a32f0ab8c04106c6248
Erik Knudsen Garcia <ek0611@gmail.com>
2024-04-12 12:18:59 +0200

    adjustments

---
+++ run/simulations.bat
@@ -0,0 +1,5 @@
+python .\simulations.py --datasource review1 --no 20 --parallel 10 --condition thursdaynight
+python .\simulations.py --datasource review1 --no 20 --parallel 10 --condition thursdaynight
+python .\simulations.py --datasource review1 --no 20 --parallel 10 --condition thursdaynight
+python .\simulations.py --datasource review1 --no 20 --parallel 10 --condition thursdaynight
+python .\simulations.py --datasource review1 --no 20 --parallel 10 --condition thursdaynight
--- run/simulations_jonas.bat
+++ run/simulations_jonas.bat
@@ -1,10 +1,3 @@
-python .\simulations.py --datasource review1 --no 10 --parallel 10 
-python .\simulations.py --datasource review1 --no 10 --parallel 10
-python .\simulations.py --datasource review1 --no 10 --parallel 10
-python .\simulations.py --datasource review1 --no 10 --parallel 10
-python .\simulations.py --datasource review1 --no 10 --parallel 10
-python .\simulations.py --datasource review1 --no 10 --parallel 10
-python .\simulations.py --datasource review1 --no 10 --parallel 10
-python .\simulations.py --datasource review1 --no 10 --parallel 10
-python .\simulations.py --datasource review1 --no 10 --parallel 10
-python .\simulations.py --datasource review1 --no 10 --parallel 10
+python .\simulations.py --datasource review1 --no 10 --parallel 10 --team_id 98027 --condition jonas
+python .\simulations.py --datasource review1 --no 10 --parallel 10 --team_id 98027 --condition jonas
+python .\simulations.py --datasource review1 --no 10 --parallel 10 --team_id 98027 --condition jonas
