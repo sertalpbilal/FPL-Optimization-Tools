b5448b7060e0ce7597dcdaa102feb60d58545faf
Erik Knudsen Garcia <ek0611@gmail.com>
2024-06-06 12:45:00 +0200

    t

--- run/simulations.bat
+++ run/simulations.bat
@@ -1,8 +1,15 @@
-python .\simulations.py --no 20 --parallel 10 --condition thursdayevening 
-python .\simulations.py --no 20 --parallel 10 --condition thursdayevening 
-python .\simulations.py --no 20 --parallel 10 --condition thursdayevening 
-python .\simulations.py --no 20 --parallel 10 --condition thursdayevening 
-python .\simulations.py --no 20 --parallel 10 --condition thursdayevening 
+python .\simulations.py --no 20 --parallel 10 --condition 2FT38 --have_2ft_in_gws 38
+python .\simulations.py --no 20 --parallel 10 --condition 2FT38 --have_2ft_in_gws 38
+python .\simulations.py --no 20 --parallel 10 --condition 2FT38 --have_2ft_in_gws 38
+python .\simulations.py --no 20 --parallel 10 --condition 2FT38 --have_2ft_in_gws 38
+python .\simulations.py --no 20 --parallel 10 --condition 2FT38 --have_2ft_in_gws 38
+
+python .\simulations.py --no 20 --parallel 10 
+python .\simulations.py --no 20 --parallel 10 
+python .\simulations.py --no 20 --parallel 10  
+python .\simulations.py --no 20 --parallel 10 
+python .\simulations.py --no 20 --parallel 10 
+ 
 
 
 
