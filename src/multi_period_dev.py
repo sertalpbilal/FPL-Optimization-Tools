838885283a60d561e2ad3f409567482ae70eb78c
Chris Musson <chris.musson@hotmail.com>
2024-11-04 17:23:46 +0000

    Fix banned_players bug

--- src/multi_period_dev.py
+++ src/multi_period_dev.py
@@ -617,8 +617,8 @@ def solve_multi_period_fpl(data, options):
     if options.get('banned', None):
         print("OC - Banned")
         banned_players = options['banned']
-        model.add_constraints((so.expr_sum(squad[p,w] for w in gameweeks) == 0 for p in banned_players), name='ban_player')
-        model.add_constraints((so.expr_sum(squad_fh[p,w] for w in gameweeks) == 0 for p in banned_players), name='ban_player_fh')
+        model.add_constraints((so.expr_sum(squad[p,w] for w in gameweeks) == 0 for p in banned_players if p in players), name='ban_player')
+        model.add_constraints((so.expr_sum(squad_fh[p,w] for w in gameweeks) == 0 for p in banned_players if p in players), name='ban_player_fh')
     
     if options.get('banned_next_gw', None):
         print("OC - Banned Next GW")
